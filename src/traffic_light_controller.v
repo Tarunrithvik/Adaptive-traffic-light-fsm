a292119a7814d9dbc72f1f74dd68fe5f5e10ca12
Tarunrithvik <b.tarunrithvik@gmail.com>
2025-07-29 22:15:44 +0530

    Create traffic_light_controller.v

---
+++ src/traffic_light_controller.v
@@ -0,0 +1,8 @@
+```verilog
+// traffic_light_controller.v
+// Adaptive 4-Way Traffic Light Controller
+// Author: Tarun Rithvik
+
+module Traffic_Light_Controller(...);
+  // Module implementation from the report
+endmodule
